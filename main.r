eed2e5c53676986e6632e97821b7bd07dd1cd59f
PabloMartinezAngerosa <pablomartinezangerosa@gmail.com>
2020-10-24 16:16:11 -0300

    we are Rich $$$$ (pobre el que lee)

--- main.r
+++ main.r
@@ -1,94 +1,77 @@
 #################################################################################
 
-        ###########################################################
-        #Deveolped by Pablo Martinez Angerosa - Vanessa Gillespie #
-        ###########################################################
+        #########################################################
+        #Deveolped by Pablo Martinez Angerosa - Vanessa Alcalde #
+        #########################################################
 
+#################################################################################
 
-
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%,        ...../%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%#               ........%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%%%%%%.                  ..........%%%%%%%%%%%%%%%%%%%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%%%%                      ...........@%%%%%%%%%%%%%%%%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%%#                       ............@@%%%%%%%%%%%%%%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%%                        .............@@@@%%%%%%%%%%%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%                         ..............@@@@@%%%%%%%%%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%                        ...............@@@@@@@@%%%%%%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%                      .................@@@@@@@@@@%%%%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%      ,@@@@@@@@@     ..@@@@@@@@@....../@@@@@@@@@@@@@%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%%    (@@@@@@@@@@    ...@@@@@@@@@@.....@@@@@@@@@@@@@@@@%%%%%
-# %%%%%%%%%%%%%%%%%%%%%%%    @@@@@@@@@#  @@@..@@@@@@@@@@....@@@@@@@@@@@@@@@@@@@@%%
-# %%%%%%%%%%%%%%%%%%%%%%%      %,      @@@@@@@....../%......@@@@@@@@@@@@@@@@@@@@@@
-# %%%%%%%%%%%%%%%%%%%%%%%             @@@@@@@@@.............@@@@@@@@@@@@@@@@@@@@@@
-# %%%%%%%%%%%%%%%%%%%%%%%%(   /       @@@@@@@@@.......%...@@@@@@@@@@@@@@@@@@@@@@@@
-# %%%%%%%%%%%%%%%%%%%%%%%%%.  @@@           ........@@@..%@@@@@@@@@@@@@@@@@@@@@@@@
-# %%%%%%%%%%%%%%%%%%%%%%%%%%  @@@            .....* .#(..%,(*%#%%%%*.%%%%%%%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%%%%%%   @@@,          .....(%,# . %%%%%%%%%%%%%%%%%%%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%%%%%%            ,%.%............*@@@@@@@@@@@@@@@@@@@@@@@@@
-# %%%%%%%%%%%%%%%%%%%%%%%%%%#                ...........@@@@@@@@@@@@@@@@@@@@@@@@@@
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%               ..........@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            .........@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%@@.      .....*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# #########%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
+############
+# librarys #
+############
 
 library(tidyverse)
-library(lubridate)
-library(nycflights13)
-library(ISLR)
+library(HH)
+library(lmtest)
+library(leaps)
+library(nortest)
+library(MASS)
+library(boot)
+library(glmulti)
 
 ################
-# source files #
+# Source files #
 ################
+
 source("databaseMerge.R")
 
+database_coins = DATABASE_COINS
+
+
+# queremos que el ultimo 70% sea de entrenamiento
+dataprice.train = database_coins[(floor(length(database_coins$btc_trend)*0.03)+1):length(database_coins$btc_trend),]
+# queremos que el primer 30 % sea d test
+dataprice.test = database_coins[1:floor(length(database_coins$btc_trend)*0.03),]
+
+
+# Buscamos todas las combinaciones con los 10 lagas de la BD y obtenemos el 
+# ajuste que tiene mejor performance.
+
+combinaciones_posibles = crossing(var1 = 0:1, var2 = 0:1, var3 = 0:1,var4 = 0:1, var5 = 0:1)
+variables_lag = c("btc_close_lag1","btc_close_lag2","btc_close_lag3","btc_close_lag4","btc_close_lag5" )
+
+lm_eval = ""
+for( variable in variables_lag[combinaciones_posibles[8,]==1]){
+  variable = paste("+ ", variable)
+  lm_eval = paste(lm_eval, variable)
+}
+
+lm_eval_first = "lmClose = lm(btc_close~ eth_open "
+lm_eval_end = ", dataprice.test)"
+lm_eval = paste(lm_eval_first,lm_eval,lm_eval_end)
+
+eval(parse(text=lm_eval))
+
+pred.dataprice <- predict(lmClose, dataprice.test, interval = "prediction")
+RMSE_dataprice = sqrt(sum((pred.dataprice[,1] - dataprice.test$btc_close )^2)/length(dataprice.test$btc_close))
+
+
+# queremos que el ultimo 70% sea de entrenamiento
+dataprice.train = database_coins[(floor(length(database_coins$btc_trend)*0.03)+1):length(database_coins$btc_trend),]
+# queremos que el primer 30 % sea d test
+dataprice.test = database_coins[1:floor(length(database_coins$btc_trend)*0.03),]
+
+lmClose = lm(btc_close~., data = dataprice.train)
+# utiliza test y predict para obtener los  RMSE
+pred.dataprice <- predict(lmClose, dataprice.test, interval = "prediction")
+# residual  
+RMSE_dataprice = sqrt(sum((pred.dataprice[,1] - dataprice.test$btc_close )^2)/length(dataprice.test$btc_close))
 
 
 
-##################
-# cargamos datos #
-##################
 
-###########
-# Bitcoin #
-###########
+crossing(var1 = 0:1, var2 = 0:1, var3 = 0:1,var4 = 0:1, var5 = 0:1)
 
-
-
-cor(bitcoin_price[,4:8])
-
-#############################
-# principales critpomonedas #
-#############################
-eth_price = read.csv("Coinbase_ETHUSD_d.csv")
-ltc_price = read.csv("Coinbase_LTCUSD_d.csv")
-bch_price = read.csv("Coinbase_BCHUSD_d.csv")
-etc_price = read.csv("Coinbase_ETCUSD_d.csv")
-link_price = read.csv("Coinbase_LINKUSD_d.csv")
-rep_price = read.csv("Coinbase_REPUSD_d.csv")
-xlm_price = read.csv("Coinbase_XLMUSD_d.csv")
-xrp_price = read.csv("Coinbase_XRPUSD_d.csv")
-
-
-#########################
-# google trends bitcion #
-#########################
-google_trends_bitcoin = read.csv("GoogleTrends_Bitcoin_d.csv")
-google_trends_bitcoin = data.frame("index"= c(1:233), "bitcoin"= google_trends_bitcoin$bitcoin[1:233] , "day" = google_trends_bitcoin$Day[1:233])
-google_trends_bitcoin = google_trends_bitcoin %>% arrange(desc(index))
-
-coins_data = data.frame("btc_trend" = google_trends_bitcoin$bitcoin[2:233], "btc_close" = btc_price$Close[1:232], "btc_open" = btc_price$Open[1:232], "btc_vol" = btc_price$Volume.BTC[2:233],"eth_open" = eth_price$Open[1:232], "eth_vol" = eth_price$Volume.ETH[2:233],"ltc_open" = ltc_price$Open[1:232], "ltc_vol" = ltc_price$Volume.LTC[2:233],"bch_open" = bch_price$open[1:232], "bch_vol" = bch_price$Volume.BCH[2:233],"etc_open" = etc_price$open[1:232], "etc_vol" = etc_price$Volume.ETC[2:233],"link_open" = link_price$open[1:232], "link_vol" = link_price$Volume.LIN[2:233],"rep_open" = rep_price$open[1:232], "rep_vol" = rep_price$Volume.REP[2:233] )
-cor(coins_data)
-
-library(HH)
 
 ########### Camino Inferencial, buscando el modelo que cumpla todas las condiciones de diagnostico 
 
@@ -182,14 +165,6 @@ summary(lm_Bitcoin_Close) # 0.06341
 step.model <- stepAIC(step.model, direction = "both", 
                       trace = FALSE)
 summary(step.model) # 543.*
-
-
-
-
-
-
-
-
 
 
 
